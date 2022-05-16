2f087f905555b11e924d1dcc1df2b42963427a5a
Ahmad Fadillah <71371202+ahmadfadillah220@users.noreply.github.com>
2022-05-16 11:00:20 +0700

    Memperbaiki Tampilan Game Page

--- progressive-sports-app/src/scripts/views/pages/gamePage.js
+++ progressive-sports-app/src/scripts/views/pages/gamePage.js
@@ -2,71 +2,14 @@
 const gamePage = {
 	async init(){
 		return `
-		<div class="relative">
-  <p class="mb-8 text-center font-bold">GAMES</p>
-  <p class="ml-5 mb-8 font-bold">Last Macth</p>
-</div>
-
-<div class="grid grid-cols-2">
-  <div class="card-league mb-6 flex">
-    <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
-    <div class="flex">
-      <div class="ml-10">
-        <h2>Espanyol</h2>
-        <h2>Villareal</h2>
-      </div>
-      <div class="ml-10">
-        <h2>2</h2>
-        <h2>2</h2>
-      </div>
-    </div>
+    <div class="relative">
+    <p class="mb-8 text-center font-bold">GAMES</p>
+    <p class="ml-20 mb-8 font-bold">Last Macth</p>
   </div>
-  <div class="card-league mb-6 flex">
-    <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
-    <div class="flex">
-      <div class="ml-10">
-        <h2>Espanyol</h2>
-        <h2>Villareal</h2>
-      </div>
-      <div class="ml-10">
-        <h2>2</h2>
-        <h2>2</h2>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="grid grid-cols-2">
-  <div class="card-league mb-6 flex">
-    <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
-    <div class="ml-10 flex">
-      <div>
-        <h2>Espanyol</h2>
-        <h2>Villareal</h2>
-      </div>
-      <div class="ml-10">
-        <h2>2</h2>
-        <h2>2</h2>
-      </div>
-    </div>
-  </div>
-  <div class="card-league mb-6 flex">
-    <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
-    <div class="ml-10 flex">
-      <div>
-        <h2>Espanyol</h2>
-        <h2>Villareal</h2>
-      </div>
-      <div class="ml-10">
-        <h2>2</h2>
-        <h2>2</h2>
-      </div>
-    </div>
-  </div>
-  <div class="">
-    <p class="ml-6 mb-8 font-bold">Next Macth</p>
-    <div class="card-league mb-6 flex">
+  
+  <div class="grid grid-cols-2">
+    <div class="card-game mb-6 ml-20 justify-between flex">
       <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
-      <div class="flex">
         <div class="ml-10">
           <h2>Espanyol</h2>
           <h2>Villareal</h2>
@@ -75,10 +18,58 @@ const gamePage = {
           <h2>2</h2>
           <h2>2</h2>
         </div>
+    </div>
+    <div class="card-game mb-6 justify-between flex">
+      <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
+        <div class="ml-10">
+          <h2>Espanyol</h2>
+          <h2>Villareal</h2>
+        </div>
+        <div class="ml-10">
+          <h2>2</h2>
+          <h2>2</h2>
+        </div>
+    </div>
+  </div>
+  <div class="grid grid-cols-2">
+    <div class="card-game mb-6 justify-between flex">
+      <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
+        <div>
+          <h2>Espanyol</h2>
+          <h2>Villareal</h2>
+        </div>
+        <div class="ml-10">
+          <h2>2</h2>
+          <h2>2</h2>
+        </div>
+    </div>
+    <div class="card-game mb-6 justify-between flex">
+      <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
+        <div>
+          <h2>Espanyol</h2>
+          <h2>Villareal</h2>
+        </div>
+        <div class="ml-10">
+          <h2>2</h2>
+          <h2>2</h2>
+        </div>
+    </div>
+    <div class="">
+      <p class="ml-20 mb-8 font-bold">Next Macth</p>
+      <div class="card-game mb-6 justify-between flex">
+        <img class="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga" />
+          <div class="ml-10">
+            <h2>Espanyol</h2>
+            <h2>Villareal</h2>
+          </div>
+          <div class="ml-10">
+            <h2>2</h2>
+            <h2>2</h2>
+          </div>
       </div>
     </div>
   </div>
-</div>
+  
 `;
 	}
 }
