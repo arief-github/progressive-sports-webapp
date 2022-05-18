21d72932b0c60246b454dc46b329c7138fe28ad4
darmawan06 <dikdik.darmawan06@gmail.com>
2022-05-18 01:01:24 -0700

    Konfigurasi Api di file FootballData dan idCompetition adalah kompetisi yang dapat digunakan di API ini

---
+++ progressive-sports-app/src/scripts/data/FootballDataApi.js
@@ -0,0 +1,58 @@
+class FootballDataApi{
+	APIKey = "";
+	baseUrl = "";
+
+	constructor(){
+		this.APIKey = "fdaae91cecbc4992be43f96cc4ba9598";
+		this.baseUrl = "https://api.football-data.org/v2";
+	}
+
+	async request(request){
+		return await $.ajax({
+			type: `GET`,
+			url: `${this.baseUrl}/${request}`,
+			headers:{
+				"X-Auth-Token":this.APIKey
+			}
+		});
+	}
+
+	async getAllMatches(){
+		let data;
+		await this.request('matches')
+			.then((response,status)=>{
+					data = (status != 'error') ? response : status;
+			});
+		return data;
+	}
+
+	async getAllCompetitions(){
+		let data;
+		await this.request('competitions/')
+			.then((response,status)=>{
+					data = (status != 'error') ? response : status;
+			});
+		return data;
+	}
+
+	async getCompetitionsById({id}){
+		let data;
+		await this.request(`competitions/${id}`)
+			.then((response,status)=>{
+					data = (status != 'error') ? response : status;
+			});
+		return data;
+	}
+
+	async getAllTeamsByIdCompetitions({id}){
+		let data;
+		await this.request(`competitions/${id}/teams`)
+			.then((response,status)=>{
+					data = (status != 'error') ? response : status;
+			});
+		return data;
+	}
+}
+
+
+export default FootballDataApi;
