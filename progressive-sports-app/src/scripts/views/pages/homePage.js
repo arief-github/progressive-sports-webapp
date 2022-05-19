22eccf4b9d4f9b87fc2f1a25c7dbd0041b77bed9
github.arief <78076941+arief-github@users.noreply.github.com>
2022-05-20 00:28:25 +0700

    showing competitions API to homepage and Update styling footer
    - Mengambil data kompetisi dengan API
    - Memperbaiki styling footer

--- progressive-sports-app/src/scripts/views/pages/homePage.js
+++ progressive-sports-app/src/scripts/views/pages/homePage.js
@@ -1,6 +1,8 @@
 import heroImage from '../components/hero-image.js';
 import header from '../components/header.js';
 import footer from '../components/footer.js';
+import FootballDataApi from '../../data/footballDataApi'
+import idCompetitions from '../../data/idCompetitions'
 
 const homePage = {
     async init() {
@@ -8,63 +10,47 @@ const homePage = {
 		<div id="hero-image"></div>
 		<h1 class="text-center mt-2 text-2xl font-bold underline  md:text-xl md:font-semibold ">LEAGUES</h1>
 		<br>
-		<div class="container flex justify-start items-strecth flex-wrap">
-			  <div class="card-league mb-6">
-			     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga">
-			    <h2>Laliga Santander</h2>
-			    <p class="mt-3">
-			      Spain
-			    </p>
-			    <p>Season 2021/2022</p>
-			  </div>
-			  <div class="card-league mb-6">
-			    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga">
-			    <h2>Laliga Santander</h2>
-			    <p class="mt-3">
-			      Spain
-			    </p>
-			    <p>Season 2021/2022</p>
-			  </div>
-			  <div class="card-league mb-6">
-			    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga">
-			    <h2>Laliga Santander</h2>
-			    <p class="mt-3">
-			      Spain
-			    </p>
-			    <p>Season 2021/2022</p>
-			  </div>
-			  <div class="card-league mb-6">
-			    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga">
-			    <h2>Laliga Santander</h2>
-			    <p class="mt-3">
-			      Spain
-			    </p>
-			    <p>Season 2021/2022</p>
-			  </div>
-			</div>
-			<h1 class="text-center mt-2 text-2xl font-bold underline md:text-xl md:font-semibold ">TOURNAMENT</h1>
-		<div class="container flex justify-start items-strecth flex-wrap">
-			  <div class="card-league mb-6">
-			    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga">
-			    <h2>Laliga Santander</h2>
-			    <p class="mt-3">
-			      Spain
-			    </p>
-			    <p>Season 2021/2022</p>
-			  </div>
-			  <div class="card-league mb-6">
-			    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/LaLiga_Santander_logo_%28stacked%29.svg/175px-LaLiga_Santander_logo_%28stacked%29.svg.png" alt="laliga">
-			    <h2>Laliga Santander</h2>
-			    <p class="mt-3">
-			      Spain
-			    </p>
-			    <p>Season 2021/2022</p>
-			  </div>
+		<div class="league-container container flex justify-end items-strecth flex-wrap">
 		</div>
 		`;
-    }, 
+    },
     async afterRender() {
         document.getElementById('hero-image').innerHTML = heroImage;
+
+        await this.renderCompetitions();
+    },
+    async renderCompetitions() {
+        let competitionsHTML = '';
+        const footballDataApi = new FootballDataApi();
+        await footballDataApi.getAllCompetitions()
+            .then((value) => {
+                value.competitions.filter((competitions) => competitions.id <= 2200 && competitions.emblemUrl !== null).slice(0,15).forEach((item) => {
+                    competitionsHTML +=
+                        `
+                          <div id="${item.id}" class="card-league mb-6 shadow-lg item-card h-[400px] bg-[#f2f2f2] rounded-[8%] shadow-lg">
+							  <div class="side-top w-full h-3/6 max-h-[50%] min-h-[50%] flex items-center bg-white p-4">
+							    <img class="mx-auto h-full" src="${item.emblemUrl}" alt="picture team">
+							  </div>
+							  <div class="side-mid flex pt-2 px-4 h-2/6 w-full items-center">
+								  <div class="description min-w-[85%] mr-4">
+								  	<h1 class="truncate text-2xl font-semibold">${item.name}</h1>
+								  	<h2 class="truncate text-lg mt-[2px] text-gray-700">${item.area.name}</h2>
+								  	<h3 class="truncate text-md mt-[2px] text-gray-700">${item.currentSeason.startDate} ${item.currentSeason.endDate}</h3>
+								  </div>
+							  </div>
+							  <div class="side-bottom w-full h-1/6 mb-2 flex justify-end">
+							  		<button class="btn-favorite rounded-[15%] w-[30%] h-5/6 bg-[#f9f9f9]  shadow-md group hover:shadow-inner">
+				             <svg class=" m-auto h-5 w-5 text-black"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="13" y1="20" x2="20" y2="13" />  <path d="M13 20v-6a1 1 0 0 1 1 -1h6v-7a2 2 0 0 0 -2 -2h-12a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7" /></svg>
+									</button>
+							  </div>
+		  					</div>
+
+						`
+						;
+                })
+
+            }).catch((err) => console.error(err))
+        document.querySelector('.league-container').innerHTML = competitionsHTML;
     }
 }
 
