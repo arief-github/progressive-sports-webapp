2a59751f9c2670de146d177e20bd7a80043ae170
rickkun11 <rickkun11@gmail.com>
2022-05-22 12:29:02 +0700

    menambahkan method dan route detail league page

--- progressive-sports-app/src/scripts/routes/routes.js
+++ progressive-sports-app/src/scripts/routes/routes.js
@@ -2,10 +2,12 @@ import FavoritePage from '../views/pages/favoritePage';
 import HomePage from '../views/pages/homePage';
 import gamePage from '../views/pages/gamePage';
 import clubPage from '../views/pages/clubPage';
+import detailLeaguePage from '../views/pages/detailLeaguePage';
 
 const routes = {
   '/': HomePage,
   '/favorite-page': FavoritePage,
+  '/detail-league-page': detailLeaguePage,
   '/game-page' : gamePage,
   '/teams/:id': clubPage,
 };
