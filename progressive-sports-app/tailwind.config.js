3628116dfc96076f5336ae5f83111de3e04a79d0
rickkun11 <rickkun11@gmail.com>
2022-05-22 12:28:35 +0700

    menambahkan style pada main.css

--- progressive-sports-app/tailwind.config.js
+++ progressive-sports-app/tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    creens: {
+      'mobile': '410px', 
+      // => @media (min-width: 410px) { ... }
+
+      'tablet': '640px', 
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
