3f5467de7f426b0a94b4256d802071ea3461b87a
Ahmad Fadillah <71371202+ahmadfadillah220@users.noreply.github.com>
2022-05-22 12:05:59 +0700

    Memperbaiki API pada game Page dan menambahkan button

--- progressive-sports-app/src/scripts/views/pages/gamePage.js
+++ progressive-sports-app/src/scripts/views/pages/gamePage.js
@@ -1,6 +1,7 @@
 import cardLastMatch from '../components/card-last-match'
 import cardNextMatch from '../components/card-next-match'
 import FootballDataApi from '../../data/footballDataApi'
+import UrlParser from '../../routes/url-parser'
 import '../components/custom-loading'
 
 const gamePage = {
@@ -11,10 +12,11 @@ const gamePage = {
             <p class="ml-20 mt-10 text-3xl mb-10 font-medium">Last Macth</p>
           </div>
           <custom-loading></custom-loading>
-          <div id="list-teams2"  class="list-teams2 w-full text-2xl grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1  xl:grid-cols-2 2xl:grid-cols-2">
+          <div id="list-teams2"  class="last-match w-full text-2xl grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1  xl:grid-cols-2 2xl:grid-cols-2">
           </div>
           <p class="ml-20 mt-10 text-3xl mb-10 font-medium">Next Macth</p>
-          <div id="next-matchs"  class="next-matchs mb-10 w-full text-2xl grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1  xl:grid-cols-2 2xl:grid-cols-2">
+          <custom-loading></custom-loading>
+          <div id="next-matchs"  class="next-match mb-10 w-full text-2xl grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1  xl:grid-cols-2 2xl:grid-cols-2">
           </div>
           
           `;
@@ -23,14 +25,20 @@ const gamePage = {
     await this.LastMatch();
     await this.nextMatch();
   },
+  async getData(){
+		const footballDataApi = new FootballDataApi();
+		const url = UrlParser.parseActiveUrlWithoutCombiner();
+		return await footballDataApi.getAllMatches({id : url.id});
+	},
   async LastMatch(){
     const footballDataApi = new FootballDataApi();
     await footballDataApi.getAllMatches()
       .then((value)=>{
       $("custom-loading").remove()
       console.log(value)
-        value.matches.filter((matches) => matches.status === "FINISHED").forEach((e) => {
-              document.querySelector('.list-teams2').innerHTML += cardLastMatch({
+      value.matches.filter((matches) => matches.status === "FINISHED").forEach((e) => {
+        document.querySelector('.last-match').innerHTML += cardLastMatch({
+                  idMatch: e.id,
                   teamOne: e.awayTeam.name,
                   teamTwo: e.homeTeam.name,
                   scoreOne: e.score.fullTime.homeTeam,
@@ -47,8 +55,9 @@ const gamePage = {
     .then((value)=>{
     $("custom-loading").remove()
     console.log(value)
-    value.matches.filter((matches) => matches.status === "SCHEDULED").forEach((e) => {
-            document.querySelector('.next-matchs').innerHTML += cardNextMatch({
+    value.matches.filter((matches) => matches.status === "SCHEDULED" && matches.score.fullTime.homeTeam === null && matches.score.fullTime.awayTeam === null).forEach((e) => {
+            document.querySelector('.next-match').innerHTML += cardNextMatch({
+                idMatch: e.id,
                 teamOne: e.awayTeam.name,
                 teamTwo: e.homeTeam.name,
                 pathImage: e.competition.area.ensignUrl,
