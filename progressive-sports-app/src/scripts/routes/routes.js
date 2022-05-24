41763dc0cbdbecba7c2c50e744fd8ee43ce4d5ca
- <74578072+darmawan06@users.noreply.github.com>
2022-05-23 20:34:16 -0700

    Perbaikan Detail League

--- progressive-sports-app/src/scripts/routes/routes.js
+++ progressive-sports-app/src/scripts/routes/routes.js
@@ -4,10 +4,19 @@ import gamePage from '../views/pages/gamePage';
 import clubPage from '../views/pages/clubPage';
 
 const routes = {
+<<<<<<< Updated upstream
   '/': HomePage,
   '/favorite-page': FavoritePage,
   '/game-page' : gamePage,
   '/teams/:id': clubPage,
+=======
+    '/': HomePage,
+    '/favorite-page': FavoritePage,
+    '/game-page': gamePage,
+    '/teams/:id': clubPage,
+    '/league/:id': detailLeaguePage,
+    '/matches/:id': detailGamePage,
+>>>>>>> Stashed changes
 };
 
 export default routes;
