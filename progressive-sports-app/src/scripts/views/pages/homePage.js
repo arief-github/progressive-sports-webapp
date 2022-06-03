e593b625b3e32fe5279293e5ba312c59b021d98f
- <74578072+darmawan06@users.noreply.github.com>
2022-06-02 17:51:11 -0700

    merapihkan tampilan home page

--- progressive-sports-app/src/scripts/views/pages/homePage.js
+++ progressive-sports-app/src/scripts/views/pages/homePage.js
@@ -10,19 +10,23 @@ const homePage = {
     async init() {
         return `
 		<div id="hero-image"></div>
-		<h1 class="text-center mt-2 text-2xl font-bold underline  md:text-xl md:font-semibold ">LEAGUES</h1>
-		<br>
-		<div class="search-league sm:ml-auto mr-6 flex w-2/4">
-						<div class="ml-auto flex items-center border border-gray-300 w-4/4 sm:2/4 shadow-inner rounded-md ">
-							<button id="btn-search-leagues" class="hover:shadow-inner m-auto flex items-center p-2">
-								<svg xmlns="http://www.w3.org/2000/svg" class="m-auto h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-								  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-								</svg>
-							</button>
-							<input type="text" id="search-league" class="w-5/6  text-xl sm:ml-auto hidden focus:outline-none" placeholder="Search leagues">
-						</div>
-		</div>
+        <div class="flex">
+            <div class="w-2/4 ml-4">
+                <h1 class="text-2xl font-bold  md:text-xl md:font-semibold ">LEAGUES</h1>
+            </div>
+            <div class="search-league sm:ml-auto mr-6 flex w-2/4">
+                <div class="ml-auto flex items-center border border-gray-300 w-4/4 sm:2/4 shadow-inner rounded-md ">
+                    <button id="btn-search-leagues" class="hover:shadow-inner m-auto flex items-center p-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="m-auto h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </button>
+                    <input type="text" id="search-league" class="w-5/6  text-xl sm:ml-auto hidden focus:outline-none" placeholder="Search leagues">
+                </div>
+            </div>
+        </div>
 		<div id="league" class="league-container  w-full h-auto p-8 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+            <custom-loading class="col-span-4"></custom-loading>
 		</div>
 		
 		`;
@@ -38,6 +42,7 @@ const homePage = {
         const footballDataApi = new FootballDataApi();
         await footballDataApi.getAllCompetitions()
             .then((value) => {
+                $("custom-loading").remove()
                 let leagues = Array();
                 idCompetitions.forEach((competition) => {
                     leagues.push(value.competitions.find(value => value.id == competition.id));
