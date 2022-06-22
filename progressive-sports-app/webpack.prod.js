acb2f1550a53f4edfbc9733474256e6453d7b47f
rickkun11 <rickkun11@gmail.com>
2022-06-22 19:18:20 +0700

    add generator optimize = false + delete babelrc

--- progressive-sports-app/webpack.prod.js
+++ progressive-sports-app/webpack.prod.js
@@ -21,6 +21,7 @@ module.exports = merge(common, {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              generatorOpts: { compact: false },
             },
           },
         ],
@@ -97,26 +98,26 @@ module.exports = merge(common, {
         },
       }),
     ],
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 70000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      automaticNameDelimiter: '~',
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
+    // splitChunks: {
+    //   chunks: 'all',
+    //   minSize: 20000,
+    //   maxSize: 20000,
+    //   minChunks: 1,
+    //   maxAsyncRequests: 30,
+    //   maxInitialRequests: 30,
+    //   automaticNameDelimiter: '~',
+    //   enforceSizeThreshold: 50000,
+    //   cacheGroups: {
+    //     defaultVendors: {
+    //       test: /[\\/]node_modules[\\/]/,
+    //       priority: -10,
+    //     },
+    //     default: {
+    //       minChunks: 2,
+    //       priority: -20,
+    //       reuseExistingChunk: true,
+    //     },
+    //   },
+    // },
   },
 });
