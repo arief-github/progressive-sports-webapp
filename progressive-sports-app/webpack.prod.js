f1bc7e66c8cf2204f4e447c233c7b4525f77b329
github.arief <78076941+arief-github@users.noreply.github.com>
2022-06-23 06:51:27 +0700

    Revert "Revert "Revert "add generator optimize = false + delete babelrc"""
    This reverts commit 0e8d25d67c3c091cac0c89ff1e401d65d725745b.

--- progressive-sports-app/webpack.prod.js
+++ progressive-sports-app/webpack.prod.js
@@ -21,7 +21,6 @@ module.exports = merge(common, {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              generatorOpts: { compact: false },
             },
           },
         ],
@@ -98,26 +97,26 @@ module.exports = merge(common, {
         },
       }),
     ],
-    // splitChunks: {
-    //   chunks: 'all',
-    //   minSize: 20000,
-    //   maxSize: 20000,
-    //   minChunks: 1,
-    //   maxAsyncRequests: 30,
-    //   maxInitialRequests: 30,
-    //   automaticNameDelimiter: '~',
-    //   enforceSizeThreshold: 50000,
-    //   cacheGroups: {
-    //     defaultVendors: {
-    //       test: /[\\/]node_modules[\\/]/,
-    //       priority: -10,
-    //     },
-    //     default: {
-    //       minChunks: 2,
-    //       priority: -20,
-    //       reuseExistingChunk: true,
-    //     },
-    //   },
-    // },
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
 });
