e0358acc6eee7c8fc711067a49dc613a4e4a49f6
- <74578072+darmawan06@users.noreply.github.com>
2022-06-23 02:52:35 -0700

    Update webpack.prod.js

--- progressive-sports-app/webpack.prod.js
+++ progressive-sports-app/webpack.prod.js
@@ -19,6 +19,7 @@ module.exports = merge(common, {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              generatorOpts: { compact: false },
             },
           },
         ],
